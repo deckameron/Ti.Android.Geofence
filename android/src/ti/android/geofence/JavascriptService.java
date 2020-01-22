0fef759e69aaa1a89a42f7daec2647f6d4de22b5
Geraldo Teixeira da Costa Neto <geraldoteixeiradacostaneto@iMac-de-Geraldo.lan>
2020-01-22 13:34:18 -0300

    Possible fix for issue #8

--- android/src/ti/android/geofence/JavascriptService.java
+++ android/src/ti/android/geofence/JavascriptService.java
@@ -4,11 +4,15 @@ import org.appcelerator.titanium.TiApplication;
 
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.JobIntentService;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class JavascriptService extends JobIntentService {
@@ -19,6 +23,26 @@ public class JavascriptService extends JobIntentService {
     public static void enqueueWork(Context context, Intent intent) {
         enqueueWork(context, JavascriptService.class, JOB_ID, intent);
     }
+    
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		if (Build.VERSION.SDK_INT >= 26) {
+			String CHANNEL_ID = "my_channel_01";
+			NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel human readable title",
+					NotificationManager.IMPORTANCE_DEFAULT);
+
+			((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+			
+			String appName = getApplicationContext().getApplicationInfo().loadLabel(getPackageManager()).toString();
+
+			Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle(appName)
+					.setContentText("Processing...").build();
+
+			startForeground(1, notification);
+		}
+	}
 	
 	@Override
 	protected void onHandleWork(Intent intent) {
